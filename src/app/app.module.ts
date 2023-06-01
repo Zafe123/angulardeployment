3e016419772cb0fd1f62d31213af2a2293bf52de
Zafe123 <scottpilgrim712@gmail.com>
2023-06-01 16:26:42 +0800

    created componets for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Material UI
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeedashboardComponent } from './employeedashboard/employeedashboard.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    EmpAddEditComponent,
+    EmployeedashboardComponent,
+    LoginComponent,
+    NavbarComponent,
+    SignupComponent,
+    UserinfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
